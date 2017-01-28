ae461b8513e5cc8db1392afb36a27bc9985dc14f
Suresh Kumar Gadi <gadisureshkumar@gmail.com>
2017-01-28 00:44:17 -0600

    Automatic push

--- public/js/script.js
+++ public/js/script.js
@@ -22,15 +22,6 @@ function carousel() {
 	setTimeout(carousel, 5000);
 }
 
-function myFunction() {
-	var x = document.getElementById("GSKNavBar");
-	if (x.className.indexOf("w3-show") == -1) {
-		x.className += " w3-show";
-	} else {
-		x.className = x.className.replace(" w3-show", "");
-	}
-}
-
 function plusDivs(n) {
 	showDivs(slideIndex += n);
 }
